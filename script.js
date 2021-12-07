edb7b0ed059ddbe2b0f77ec60f13476a03f4ae44
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2021-12-07 22:17:52 +0530

    Implemented Toggle Task Functionality

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-(function(){
+
     let tasks = [];
     const taskInput = document.getElementById('todo-input');
     const tasksCounter = document.getElementById('task-counter');
     const tasksList = document.getElementById('tasks-list');
 
     //Function which adds task
-    function addTask(task){
-        if(task){
+    function addTask(task) {
+        if (task) {
             tasks.push(task);
             taskInput.value = "";
             renderList();
@@ -15,8 +15,8 @@
     }
 
     //Function which deletes the task
-    function deleteTask(taskId){
-        if(taskId){
+    function deleteTask(taskId) {
+        if (taskId) {
             tasks = tasks.filter(function (task) {
                 return task.id !== taskId;
             });
@@ -26,46 +26,55 @@
     }
 
     //Function which toggle's task status
-    function toggleTask(task){
+    function toggleTask(taskId) {
+        if (taskId) {
+            let task = tasks.filter(function (task) {
+                return task.id === taskId;
+            });
 
+            const currentTask = task[0];
+            currentTask.completed = !currentTask.completed;
+            renderList();
+            showNotification('Task Toggled Successfully');
+        }
     }
 
     //Function to mark all tasks as Complete
-    function markAllTaskAsComplete(){
+    function markAllTaskAsComplete() {
 
     }
 
     //Function to remove completed Tasks
-    function removeCompletedTasks(){
+    function removeCompletedTasks() {
 
     }
 
     //Function to show Notifications
-    function showNotification(text){
+    function showNotification(text) {
 
     }
 
     //Function which renders the List
-    function renderList(){
-        if(tasks.length !== 0){
+    function renderList() {
+        if (tasks.length !== 0) {
             tasksList.innerHTML = "";
-            for(let i = 0; i < tasks.length; i++){
+            for (let i = 0; i < tasks.length; i++) {
                 const currentTask = tasks[i];
                 addTaskToDOM(currentTask);
             }
-        }else{
+        } else {
             tasksList.innerHTML = "<div class='dimmed center'>No Tasks Pending</div>";
         }
 
     }
 
     //Function which adds the task to DOM
-    function addTaskToDOM(task){
+    function addTaskToDOM(task) {
         const li = document.createElement('li');
         li.classList.add('task');
         li.innerHTML = `
             <div>
-                <input type="checkbox" ${ task.completed ? 'checked' : ''} id="${task.id}" class="custom-checkbox">
+                <input type="checkbox" id="${task.id}" class="custom-checkbox" ${task.completed ? 'checked' : ''}>
                 <label for="${task.id}">${task.title}</label>
             </div>
             <div class="delete-task" data-id="${task.id}"></div>
@@ -76,8 +85,8 @@
     }
 
     //Function which handles the Keyup Event, and performs adding of task to the list
-    function handleTaskAddEvent(e){
-        if(e.key === 'Enter'){
+    function handleTaskAddEvent(e) {
+        if (e.key === 'Enter') {
             const title = e.target.value;
 
             const task = {
@@ -90,17 +99,18 @@
         }
     }
 
-    function handleClickEvent(e){
+    function handleClickEvent(e) {
 
         const target = e.target;
-        if(target.className === 'delete-task'){
+        if (target.className === 'delete-task') {
             const taskId = Number(target.dataset.id);
             deleteTask(taskId);
+        } else if (target.className === 'custom-checkbox') {
+            const taskId = Number(target.id);
+            toggleTask(taskId);
         }
     }
 
     taskInput.addEventListener('keyup', handleTaskAddEvent);
     document.addEventListener('click', handleClickEvent);
     renderList();
-
-})()
