8bd6235c9529f1070306ab3ab826d6552eb5da6f
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2021-12-07 21:01:02 +0530

    Implemented Add Task Functionality

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 (function(){
-    let tasks;
+    let tasks = [];
     const taskInput = document.getElementById('todo-input');
     const tasksCounter = document.getElementById('task-counter');
+    const tasksList = document.getElementById('tasks-list');
 
     //Function which adds task
     function addTask(task){
-
+        if(task){
+            tasks.push(task);
+            taskInput.value = "";
+            renderList();
+            showNotification('Task Added Successfully');
+        }
     }
 
     //Function which deletes the task
@@ -29,18 +35,50 @@
     }
 
     //Function to show Notifications
-    function showNotification(){
+    function showNotification(text){
 
     }
 
     //Function which renders the List
     function renderList(){
-
+        tasksList.innerHTML = "";
+        for(let i = 0; i < tasks.length; i++){
+            const currentTask = tasks[i];
+            addTaskToDOM(currentTask);
+        }
     }
 
     //Function which adds the task to DOM
     function addTaskToDOM(task){
+        const li = document.createElement('li');
+        li.classList.add('task');
+        li.innerHTML = `
+            <div>
+                <input type="checkbox" ${ task.completed ? 'checked' : ''} id="${task.id}" class="custom-checkbox">
+                <label for="${task.id}">${task.title}</label>
+            </div>
+            <div class="delete-task" data-id="${task.id}"></div>
+        `;
+        tasksList.append(li);
+        tasksCounter.innerHTML = tasks.length;
 
     }
+
+    function handleTaskAddEvent(e){
+        if(e.key === 'Enter'){
+            const title = e.target.value;
+
+            const task = {
+                id: Date.now(),
+                title: title,
+                completed: false,
+            }
+
+            addTask(task);
+        }
+    }
+
+    taskInput.addEventListener('keyup', handleTaskAddEvent);
+    renderList();
 
 })()
