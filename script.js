3ffdbf8bea0deb27a10cc42a435410027e425121
Vivek Bora <39134607+BoraVivek@users.noreply.github.com>
2021-12-08 20:19:33 +0530

    Adding Comments to the code

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 (function () {
     let tasks = [];
-    const taskInput = document.getElementById('todo-input');
-    const tasksCounter = document.getElementById('task-counter');
-    const tasksList = document.getElementById('tasks-list');
+    const taskInput = document.getElementById('todo-input'); //TODO Input Box
+    const tasksCounter = document.getElementById('task-counter'); //Task Counter
+    const tasksList = document.getElementById('tasks-list'); //Tasks List where tasks are rendered
+    let activeFilter = "all"; //Manages the Current Filter of Tasks
 
     //Function which adds task
     function addTask(task) {
         if (task) {
-            tasks.push(task);
-            taskInput.value = "";
-            renderList();
+            tasks.push(task); //Add task to the array
+            taskInput.value = ""; //Resetting the value of input box
+            renderList(activeFilter);
             showNotification('Task Added Successfully');
         }
     }
@@ -20,7 +21,7 @@
             tasks = tasks.filter(function (task) {
                 return task.id !== taskId;
             });
-            renderList();
+            renderList(activeFilter);
             showNotification("Task Deleted Successfully");
         }
     }
@@ -33,8 +34,8 @@
             });
 
             const currentTask = task[0];
-            currentTask.completed = !currentTask.completed;
-            renderList();
+            currentTask.completed = !currentTask.completed; //Toggling the completed status of task
+            renderList(activeFilter);
             showNotification('Task Toggled Successfully');
         }
     }
@@ -45,7 +46,7 @@
             return task.completed=true;
         })
 
-        renderList();
+        renderList(activeFilter);
         showNotification('All tasks are marked as completed');
     }
 
@@ -55,7 +56,7 @@
             return task.completed !== true;
         });
 
-        renderList();
+        renderList(activeFilter);
         showNotification('Completed Tasks are cleared');
     }
 
@@ -69,6 +70,7 @@
         if (tasks.length !== 0) {
             tasksList.innerHTML = "";
 
+            //Check for filters and based on selected filter render the tasks.
             switch(taskFilter){
                 case 'all':
                     taskRenderHandler(tasks);
@@ -77,6 +79,8 @@
                     const uncompletedTasks = tasks.filter(function(task){
                         return task.completed === false;
                     });
+
+                    //If no uncompleted task present show message
                     if(uncompletedTasks.length === 0){
                         tasksList.innerHTML = "<div class='dimmed center'>No Uncompleted Tasks</div>";
                         return;
@@ -88,6 +92,7 @@
                         return task.completed === true;
                     });
 
+                    //If no completed task present, show message
                     if(completedTasks.length === 0){
                         tasksList.innerHTML = "<div class='dimmed center'>No Completed Task</div>";
                         return;
@@ -103,16 +108,18 @@
     }
 
     //Function for optimizing the code, and removing repetitive tasks by making a middle handler
+    //It loops through all tasks, and passes each task to addTaskToDOM function which than adds the task to DOM
     function taskRenderHandler(tasks){
         for (let i = 0; i < tasks.length; i++) {
             const currentTask = tasks[i];
-            addTaskToDOM(currentTask);
-            taskCounter();
+            addTaskToDOM(currentTask); //Add Task to the DOM
+            taskCounter(); //Update Task Counter
         }
     }
 
     //Function which adds the task to DOM
     function addTaskToDOM(task) {
+        //Creating new LI Element
         const li = document.createElement('li');
         li.classList.add('task');
         li.innerHTML = `
@@ -122,7 +129,7 @@
             </div>
             <div class="delete-task" data-id="${task.id}"></div>
         `;
-        tasksList.append(li);
+        tasksList.append(li); //Appending the created LI to the tasksList in dom
     }
 
     //Function to Count the no. of pending tasks and add them in DOM
@@ -137,36 +144,40 @@
         if (e.key === 'Enter') {
             const title = e.target.value;
 
+            //Creating Task Object
             const task = {
                 id: Date.now(),
                 title: title,
                 completed: false,
             }
-            addTask(task);
+            addTask(task); //Calling add task function and passing the task object as argument.
         }
     }
 
+    //Function which handles the click Event and performs some tasks based on the target => Event Delegation is used here to optimize the events
     function handleClickEvent(e) {
-
         const target = e.target;
         if (target.className === 'delete-task') {
             const taskId = Number(target.dataset.id);
-            deleteTask(taskId);
+            deleteTask(taskId); //Calling Delete Task Function
         } else if (target.className === 'custom-checkbox') {
             const taskId = Number(target.id);
-            toggleTask(taskId);
+            toggleTask(taskId); //Calling Toggle Task Function
         }else if(target.id === 'complete-tasks'){
-            markAllTaskAsComplete();
+            markAllTaskAsComplete(); //Calling Mark All Task as Complete Function
         }else if(target.id === 'clear-complete'){
-            removeCompletedTasks();
+            removeCompletedTasks(); //Calling Remove Completed Task Function
         }else if(target.id === 'all-tasks'){
             toggleActive(target);
+            activeFilter = "all"; //Changing the active filter to all
             renderList('all');
         }else if(target.id === 'completed-tasks'){
             toggleActive(target);
+            activeFilter = "completed"; //Changing the active filter to completed
             renderList('completed');
         }else if(target.id === 'uncompleted-tasks'){
             toggleActive(target);
+            activeFilter = "uncompleted"; //Changing the active filter to uncompleted
             renderList('uncompleted');
         }
     }
@@ -174,6 +185,7 @@
     //Function which toggle's the Active State of Task Filters
     function toggleActive(target){
         target.classList.add('active');
+        //Remove Active class from in-active filters
         if(target.id === 'all-tasks'){
             document.getElementById('uncompleted-tasks').classList.remove('active');
             document.getElementById('completed-tasks').classList.remove('active');
@@ -190,7 +202,7 @@
     function initializeApp(){
         taskInput.addEventListener('keyup', handleTaskAddEvent);
         document.addEventListener('click', handleClickEvent);
-        renderList();
+        renderList(activeFilter);
     }
 
     initializeApp();
